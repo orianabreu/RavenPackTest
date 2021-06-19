a43ccdc1d8db4995ccf5e53d335be3788d1a8180
Oriana Abreu <oriabreu.v@gmail.com>
2021-06-20 01:40:30 +0200

    create post component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import theme from './styles/theme';
 import NavBar from './components/NavBar/NavBar';
 import Home from './components/Home/Home';
+import Post from './components/Post/Post';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <CssBaseline />
         <NavBar />
         <Home />
+        <Post />
     </ ThemeProvider>
   );
 }
---
+++ src/components/Post/styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    postCard: {
+        maxWidth: 345,
+        margin: '80px',
+    }
+  }));
+
+  export default useStyles;
