e7f6e3668a50356fa4ba1ed679aa0dc8597941b9
Oriana Abreu <oriabreu.v@gmail.com>
2021-06-24 13:17:30 +0200

    add menu button to navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,23 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import { Typography,
+  Menu,
+  MenuItem
+} from '@material-ui/core';
 
 import useStyles from './styles';
+import Button from '../Button/Button';
 
 export default function NavBar() {
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+  setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   const {container, title, MuiAppBar } = useStyles();
 
   return (
     <div className={container}>
       <AppBar position="static" className={MuiAppBar}>
         <Toolbar>
-          <Link to='/' style={{ textDecoration: 'none'}}>
             <Typography className={title}>
               Blog
             </Typography>
-          </Link>
+
+            <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                Menu
+            </Button>
+            <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+            >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+                <MenuItem onClick={handleClose}>Logout</MenuItem>
+            </Menu>
         </Toolbar>
       </AppBar>
     </div>
--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -11,7 +11,7 @@ const useStyles = makeStyles((theme) => ({
     },
     MuiAppBar: {
         backgroundColor: 'transparent',
-    },
+    }
   }));
 
   export default useStyles;
